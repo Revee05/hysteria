8aaadd6fa4086a4a8b21b427a220c6af016e597b
Revee05 <rfk1000002@gmail.com>
2026-01-24 19:19:04 +0700

    feat: update header logo and enhance icon components
    - Replaced the logo in Header component with a new SVG logo.
    - Adjusted logo dimensions for better fit.
    - Added volume control icon with mute/unmute functionality in icon component.
    - Updated social media icons to use currentColor for better theming support.
    - Introduced a new SVG file for the logo in the public/svg directory.

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,10 @@ import ArtikelHysteria from "../_sectionComponents/halaman_utama/artikel_hysteri
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-zinc-900 text-white font-sans dark:bg-black">
+    <div className="flex flex-col bg-white text-zinc-900 font-sans dark:bg-white dark:text-zinc-900">
       <Hero />
-      {/* <div className="relative w-full max-w-[1920px] lg:w-[1920px] mx-auto flex flex-col items-center justify-start py-32 px-16 bg-[#0b1220] sm:items-start opacity-100">
-        <PlatformKami />
-        <ArtikelHysteria />
-      </div> */}
+      {/* <PlatformKami />
+      <ArtikelHysteria /> */}
     </div>
   );
 }
